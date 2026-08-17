ebcb195fb16c47a409b62aca323832e0604f0e29
adriano-poci <adriano.poci@students.mq.edu.au>
2026-08-17 15:25:30 +0100

    autosave: CubeFit on aslxlap31 (main) @ 2026-08-17 15:25:30 +0100

--- kz_2Fit.sh
+++ kz_2Fit.sh
@@ -12,14 +12,14 @@
 #SBATCH -D "/data/phys-gal-dynamics/phys2603/muse"
 #SBATCH --output="/data/phys-gal-dynamics/phys2603/CubeFit/log_2Fit.log" --open-mode=append
 #SBATCH --error="/data/phys-gal-dynamics/phys2603/CubeFit/log_2Fit.log" --open-mode=append
-#SBATCH -p medium
+#SBATCH -p long
 
 #SBATCH --job-name="CubeFit_2Fit"
-#SBATCH --time=2-00:00
+#SBATCH --time=6-00:00
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --mem=512G
+#SBATCH --mem=700G
 #SBATCH --hint=nomultithread
 #SBATCH --exclusive
 #SBATCH --mail-type=ALL
