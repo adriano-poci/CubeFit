bce33db573fcfb574c35104ac1acddc46ce56ffb
Adriano Poci <poci39@gmail.com>
2026-09-06 23:04:38 +0100

    autosave: CubeFit on acerMint (main) @ 2026-09-06 23:04:38 +0100

--- kz_rio.py
+++ kz_rio.py
@@ -87,6 +87,10 @@ def main():
 
     if args.ncomp is not None:
         propDict['nCuts'] = args.ncomp
+    if propDict['nCuts'] > 50:
+        propDict['sspIdx'] = ([-1.5, -1.0, -0.6, -0.3, 0.0, 0.15, 0.26, 0.4],
+            [3.0, 6.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0],
+            [-0.2, 0.0, 0.2, 0.4, 0.6])
     print(propDict)
 
     try:
